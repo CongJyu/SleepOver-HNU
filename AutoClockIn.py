7cd1636a484df080f28a0068dac88418ff123448
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-01-26 09:37:48 +0800

    Fixed some problems

--- AutoClockIn.py
+++ AutoClockIn.py
@@ -6,7 +6,8 @@ import os
 headers_1 = {
     "Cookie": "arccount62298=c; arccount62019=c",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66"
-}
+}  # 验证码token爬取
+
 headers_2 = {
     "Accept": "application/json, text/plain, */*",
     "Accept-Encoding": "gzip, deflate, br",
@@ -20,19 +21,21 @@ headers_2 = {
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75"
-}
-headers_3 = {
-    "Host": "cloud.baidu.com",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.76",
-    "Accept": "*/*",
-    "Origin": "https://cloud.baidu.com",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Dest": "empty",
-    "Referer": "https://cloud.baidu.com/product/ocr/general",
-    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
-}
+}  # 登录及打卡
 
+headers_3 = {
+    'Host': 'cloud.baidu.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.76',
+    'Accept': '*/*',
+    'Origin': 'https://cloud.baidu.com',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://cloud.baidu.com/product/ocr/general',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+}  # 百度OCR识别验证码
+
+# 获取变量
 usr = os.getenv("usr")
 pwd = os.getenv("pwd")
 RealAddress = os.getenv("RealAddress")
@@ -42,19 +45,27 @@ RealCounty = l[2]
 RealProvince = l[0]
 
 
-def clock_in():
+# step 1: 获取验证码Token及图片
+
+def ClockIn():
     try:
         token_json = requests.get("https://fangkong.hnu.edu.cn/api/v1/account/getimgvcode", headers=headers_1)
+
         if token_json.status_code != 200:
             print("Token爬取失败，正在重试")
         while token_json.status_code != 200:
             token_json = requests.get("https://fangkong.hnu.edu.cn/api/v1/account/getimgvcode", headers=headers_1)
+
         data = json.loads(token_json.text)
         token = data["data"]["Token"]
+
         img_url = "https://fangkong.hnu.edu.cn/imagevcode?token=" + token
         with open("img.jpg", "wb") as img:
             img.write(requests.get(img_url).content)
-        with open("img.jpg", "rb") as f:
+
+        # 解析验证码
+
+        with open("img.jpg", 'rb') as f:
             img = base64.b64encode(f.read())
         data = {
             'image': 'data:image/jpeg;base64,' + str(img)[2:-1],
@@ -62,23 +73,31 @@ def clock_in():
             'type': 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic',
             'detect_direction': 'false'
         }
+
         response = requests.post('https://cloud.baidu.com/aidemo', headers=headers_3, data=data)
         result = response.json()['data']['words_result'][0]['words']
+
+        # step 2: 模拟登录操作
         data = {
             "Code": usr,
             "Password": pwd,
             "Token": token,
             "VerCode": result
         }
+
         session = requests.Session()
         response = session.post("https://fangkong.hnu.edu.cn/api/v1/account/login", headers=headers_2,
                                 data=json.dumps(data))
-        in_campus = response.json()["data"]["IsShowBackCampus"]
-        if response.json()["Code"] != 0:
+
+        InCampus = response.json()["data"]["IsShowBackCampus"]
+
+        if response.json()["code"] != 0:
             print("验证码错误")
-            clock_in()
+            ClockIn()
         else:
-            data1 = {
+            # step 3: 模拟打卡操作
+
+            data1 = {  # 假期版本
                 # "Temperature":null,
                 "RealProvince": RealProvince,
                 "RealCity": RealCity,
@@ -103,7 +122,8 @@ def clock_in():
                 "dailyinfo": {
                     "IsVia": "0",
                     "DateTrip": ""
-                }, "IsInCampus": "0",
+                },
+                "IsInCampus": "0",
                 "IsViaHuBei": "0",
                 "IsViaWuHan": "0",
                 "InsulatedAddress": "",
@@ -112,7 +132,8 @@ def clock_in():
                 # "Longitude":null,
                 # "Latitude":null
             }
-            data2 = {
+
+            data2 = {  # 上学期间版本
                 "BackState": 1,
                 "MorningTemp": "36.5",
                 "NightTemp": "36.5",
@@ -122,20 +143,22 @@ def clock_in():
                 "RealProvince": RealProvince,
                 "tripinfolist": []
             }
-            if in_campus:
-                print("在校")
+
+            if (InCampus):
+                print('在校')
                 response = session.post("https://fangkong.hnu.edu.cn/api/v1/clockinlog/add", headers=headers_2,
                                         data=json.dumps(data2))
             else:
-                print("离校")
+                print('离校')
                 response = session.post("https://fangkong.hnu.edu.cn/api/v1/clockinlog/add", headers=headers_2,
                                         data=json.dumps(data1))
+
             msg = response.json()["msg"]
             print(msg)
     except:
         print("Error")
-        clock_in()
+        ClockIn()
 
 
-if __name__ == "__main__":
-    clock_in()
+if __name__ == '__main__':
+    ClockIn()
